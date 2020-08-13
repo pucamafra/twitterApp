24a7018094892d473b382483baf0fef77839db3c
Marlon Mafra <marlon.mafra@walmartlabs.com>
2020-08-13 19:20:23 -0300

    Remove unused file

---
+++ domain/src/main/java/com/marlonmafra/domain/common/Const.kt
@@ -0,0 +1,5 @@
+package com.marlonmafra.domain.common
+
+object Const {
+    const val OAUTH_TOKEN_PARAM = "oauth_token="
+}
--- domain/src/main/java/com/marlonmafra/domain/model/RequestTokenResponse.kt
+++ domain/src/main/java/com/marlonmafra/domain/model/RequestTokenResponse.kt
@@ -1,10 +1,9 @@
 package com.marlonmafra.domain.model
 
+import com.marlonmafra.domain.common.Const.OAUTH_TOKEN_PARAM
+
 class RequestTokenResponse(rawResponse: String) {
 
     private val array: List<Any> = rawResponse.split('&')
-    val oauthToken: String = (array[0] as String).removePrefix("oauth_token=")
-    val oauthTokenSecret: String = (array[1] as String).removePrefix("oauth_token_secret=")
-    val oauthCallbackConfirmed: Boolean =
-        (array[2] as String).removePrefix("oauth_callback_confirmed=").toBoolean()
+    val oauthToken: String = (array[0] as String).removePrefix(OAUTH_TOKEN_PARAM)
 }
