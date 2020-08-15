0ab531e72dc974f59c7522e58636bd4821fcc83f
Marlon Mafra <marlon.mafra@walmartlabs.com>
2020-08-14 23:22:59 -0300

    Move coordinate generation to repository

--- app/src/main/java/com/marlonmafra/twitterapp/features/home/MainInteractor.kt
+++ app/src/main/java/com/marlonmafra/twitterapp/features/home/MainInteractor.kt
@@ -2,12 +2,10 @@ package com.marlonmafra.twitterapp.features.home
 
 import com.marlonmafra.data.repository.authentication.AuthenticationRepository
 import com.marlonmafra.data.repository.twitter.TwitterRepository
-import com.marlonmafra.domain.model.Coordinate
 import com.marlonmafra.domain.model.Tweet
 import io.reactivex.Completable
 import io.reactivex.Single
 import javax.inject.Inject
-import kotlin.random.Random
 
 class MainInteractor @Inject constructor(
     private val twitterRepository: TwitterRepository,
@@ -16,24 +14,6 @@ class MainInteractor @Inject constructor(
 
     fun fetchHomeTimeline(): Single<List<Tweet>> {
         return twitterRepository.fetchHomeTimeline()
-            .map { tweetList ->
-                tweetList.map { it.coordinates = generateCoordinate() }
-                tweetList
-            }
-    }
-
-    private fun generateCoordinate(): Coordinate {
-        val latitude = generateLatitude()
-        val longitude = generateLongitude()
-        return Coordinate(arrayOf(latitude, longitude))
-    }
-
-    private fun generateLatitude(): Double {
-        return Random.nextDouble(-90.0, 90.0)
-    }
-
-    private fun generateLongitude(): Double {
-        return Random.nextDouble(-180.0, 180.0)
     }
 
     fun logout(): Completable {
--- data/src/main/java/com/marlonmafra/data/repository/twitter/remote/TwitterRemoteDataSource.kt
+++ data/src/main/java/com/marlonmafra/data/repository/twitter/remote/TwitterRemoteDataSource.kt
@@ -1,9 +1,11 @@
 package com.marlonmafra.data.repository.twitter.remote
 
 import com.marlonmafra.data.rest.ApiService
+import com.marlonmafra.domain.model.Coordinate
 import com.marlonmafra.domain.model.Tweet
 import io.reactivex.Single
 import javax.inject.Inject
+import kotlin.random.Random
 
 class TwitterRemoteDataSource @Inject constructor(
     private val apiService: ApiService
@@ -11,5 +13,24 @@ class TwitterRemoteDataSource @Inject constructor(
 
     override fun fetchHomeTimeline(): Single<List<Tweet>> {
         return apiService.homeTimeline()
+            .map { tweetList ->
+                tweetList.map { it.coordinates = generateCoordinate() }
+                tweetList
+            }
     }
+
+    private fun generateCoordinate(): Coordinate {
+        val latitude = generateLatitude()
+        val longitude = generateLongitude()
+        return Coordinate(arrayOf(latitude, longitude))
+    }
+
+    private fun generateLatitude(): Double {
+        return Random.nextDouble(-90.0, 90.0)
+    }
+
+    private fun generateLongitude(): Double {
+        return Random.nextDouble(-180.0, 180.0)
+    }
+
 }
